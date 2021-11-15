a0ffdcfcf8872564334cba18d0dd03efc68e4b2b
Benjamin Hughes <behu@kea.dk>
2021-11-15 19:31:07 +0100

    asd

--- code-written/introduction/introduction.py
+++ code-written/introduction/introduction.py
@@ -1,1 +1,32 @@
-print("hej")
+# this line prints the string hello world to the console
+print("Hello World")
+# prints 8
+print(8)
+print(8.5)
+
+print(8.7)
+
+
+# print(10/0)
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print('he')
+print(1, 2, 3, 4, 5)
+
+age = 22
+print("age is: ", age)
+age = 34
+print(age)
+
+print(2 * 20)
+print(10 - 9)
+
+sum_of_numbers = 10 + 30
+print(sum_of_numbers)
+sum_of_numbers = "test"
+print(sum_of_numbers)
