fa37fec406c5223e7fac497df4e94e8b43ea1889
Benjamin Hughes <behu@kea.dk>
2021-11-22 19:41:40 +0100

    asd

---
+++ code-written/strings/strings.py
@@ -0,0 +1,66 @@
+# crypto converter feedback
+print(round(0.251235614236541263, 2))
+amount_of_bitcoin = 0.23
+#print("Amount of bitcoin", amount_of_bitcoin)
+#print(f"Amount of bitcoin {amount_of_bitcoin}")
+
+name = "benjamin"
+age = 23
+#print("my name is ", name, " and i am ", age, " years old")
+#print(f"my name is {name} and i am {age} years old")
+
+bitcoin_per_dollars = 0.00232323
+amount_of_dollars = 10
+# print("The amount of bitcoin is", bitcoin_per_dollars * amount_of_dollars)
+amount_of_bitcoin = bitcoin_per_dollars * amount_of_dollars
+# print("The amount of bitcoin is", amount_of_bitcoin)
+
+
+# String
+#age = input("Please put in your age")
+#print(age)
+
+#name = input("Please put in your name")
+#print(name)
+
+# benjamin's age '''\\\
+print("benjamin\'s age")
+
+print("asdasdasd"
+      "asdasdasd"
+      "asdasd")
+
+# paragraph
+print("""asd
+asd
+asd""")
+
+name = "per"
+print(name[0])
+print(name[2])
+
+
+print(len(name))
+hello_string = "hello "
+benjamin_string = "benjamin"
+age = 34
+# Hello benjamin i am 34 years old
+print(hello_string + benjamin_string + "i am " + str(age))
+
+hejsa_string = "hejsa"
+print(hejsa_string)
+print(hejsa_string[0:3])
+print(hejsa_string[:3])
+print(hejsa_string[:])
+print(hejsa_string[3:])
+print(hejsa_string[-1])
+print(hejsa_string[-2:])
+
+print(hejsa_string.upper())
+print(hejsa_string)
+
+weird_formatted_text = "    asdasd     "
+print(weird_formatted_text)
+print(weird_formatted_text.strip())
+print(weird_formatted_text.lstrip())
+print(weird_formatted_text.rstrip())
--- lectures/week-47.md
+++ lectures/week-47.md
@@ -4,7 +4,7 @@
 
 [Join her](https://teams.microsoft.com/l/meetup-join/19%3aw330wMm3u592zdz4EbpSEHRvRVjDfRFH_6TWUiY9m-A1%40thread.tacv2/1637596204654?context={"Tid"%3a"d10c3c6e-c228-4944-8b6a-6067c6afe3c9"%2c"Oid"%3a"32e4ca2f-6ba3-4bdf-bb5f-7414a5ae6d0b"})
 
-
+https://behu.gitbook.io/python-basics/lectures/week-47
 
 ## Todays program
 
@@ -108,7 +108,7 @@ Do the following exercises from the book 👇
 
 ### Exercise 2
 
-Create a variable with your name. If your name contains the character `e` then log out the string `YOUR_NAME contains e`. If not the log out `YOUR_NAME does not contain e`. 
+Create a variable with your name. If your name contains the character `e` then print the string `YOUR_NAME contains e`. If not the log out `YOUR_NAME does not contain e`. 
 
 
 
