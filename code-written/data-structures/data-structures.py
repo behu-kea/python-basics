c3a429a19dec14553ef8f4b62bd6e9435e923cd6
Benjamin Hughes <behu@kea.dk>
2022-01-03 19:23:29 +0100

    asd

--- code-written/data-structures/data-structures.py
+++ code-written/data-structures/data-structures.py
@@ -28,3 +28,78 @@ print(names[1:5])
 print(len(names))
 names[0] = "Hans"
 print(names)
+# Tilføj et enkelt element i slutningen af listen
+names.append("Tommy")
+print(names)
+# fjern et enkelt element ved et bestemt index
+names.pop(0)
+print(names)
+
+surnames = ["Hansen", "Jensen"]
+# Tilføjer vi en anden liste i slutningen af listen
+names.extend(surnames)
+print(names)
+# se hvordan man kan bruge metoder på en bestemt datatype
+#print(help(names))
+
+#
+#for i in range(3):
+    #print(i)
+
+
+for name in names:
+    print(name)
+
+numbers = [1, 2, 3, 4]
+print(numbers)
+# 2, 4, 6, 8
+# Mappe
+number_doubled = [x * 2 for x in numbers]
+print(number_doubled)
+
+prices = [100, 1000, 10]
+prices.sort(reverse = True)
+print(prices)
+
+print(400 in prices)
+print(100 in prices)
+
+
+# Dictionary
+# key, value store
+hans_student = {
+    "name": "Hans",
+    "age": 23,
+    "grades": [5, 7, 12, 12]
+}
+
+yvonne_student = {
+    "name": "Yvonne",
+    "age": 27,
+    "grades": [5]
+}
+
+print(hans_student)
+print(hans_student["name"])
+print(hans_student["age"])
+print(hans_student["grades"])
+hans_student["name"] = "Per"
+print(hans_student["name"])
+print(hans_student)
+del hans_student["name"]
+# print(hans_student)
+# excel, database
+
+students = [hans_student, yvonne_student]
+for student in students:
+    print(student)
+
+
+municipality_inhabitants = {
+    "helsingør": 23300,
+    "københavn": 12998,
+}
+print(municipality_inhabitants["københavn"])
+municipality_inhabitants["helsingør"] = 1234
+print(municipality_inhabitants)
+
